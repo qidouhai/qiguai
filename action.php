c9bb7a1debdc7861664f280403a8a1124b59af55
qidouhai <qidouhai@sohu.com>
2022-11-06 16:34:56 +0800

    Create action.php
    接收数据

---
+++ action.php
@@ -0,0 +1,68 @@
+<?php
+header('content-type:text/html;charset=utf-8');   
+//接收内容
+
+$urltype = $_POST["urltype"];  //站点/小程序/app的类型 （1 网站 2 小程序 3 app ）
+
+$upjson =  $_POST["upjson"];    //json网址链接
+
+$upjsonsite =  $_POST["upjsonsite"];  //站点网址链接
+
+$upjsonsitename =  $_POST["upjsonsitename"];   //站点名称
+
+
+
+//url验证
+//$url = $_POST["upjson"];
+//1 地址接近正确
+//2 url 地址不正确
+function or_url($url){
+if (filter_var($url, FILTER_VALIDATE_URL) !== false) {
+
+return 1;
+}else{
+
+return 2;
+}
+
+}
+
+$urlzhuangtai_1=or_url($upjson);
+$urlzhuangtai_2=or_url($upjsonsite);
+
+
+
+
+
+if ($urlzhuangtai_1==1||$urlzhuangtai_2==1){
+echo "你提交的内容如下，请检查是否有误</br>";
+if ($urltype==1){
+echo  "类型：网站"."</br>";
+}elseif($urltype==2){
+echo  "类型：小程序"."</br>";
+}else{
+echo  "类型：APP"."</br>";
+}
+ echo "JSON：".$upjson."</br>";
+echo "网址：".$upjsonsite."</br>";
+echo "网站名称：".$upjsonsitename."</br>";
+
+echo<<<EOT
+<a href="javascript:history.back(-1)" class="dropdown-toggle">
+               返回修改
+              </a>
+EOT;
+echo<<<EOT
+<form action="/reok.php" method="post">
+<label><input name="reok" type="hidden" value="ok" /></label> 
+<input type="submit" value="确认提交" class="button"></form>
+EOT;
+}else{
+echo "url不正确";
+echo<<<EOT
+<a href="javascript:history.back(-1)" class="dropdown-toggle">
+               返回
+              </a>
+EOT;
+}
+?>

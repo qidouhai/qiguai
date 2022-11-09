87042132d1fd58c41de78e9d3bdc2f7d9d5b2097
qidouhai <qidouhai@sohu.com>
2022-11-09 14:41:04 +0800

    Update sqlcon.php
    修改错误

--- sqlcon.php
+++ sqlcon.php
@@ -2,7 +2,7 @@
  
 	$conn=mysqli_connect("localhost","qiguai","123456") or die("数据库连接错误");
 	
-	$select = mysqli_select_db($conn,"urltablefirst");
+	$select = mysqli_select_db($conn,"qiguai");
 	
 	mysqli_query($conn,"set names 'utf8'");
  ?>
